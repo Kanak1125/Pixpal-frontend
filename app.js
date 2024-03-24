63ee8c51f861cec2b47c6acd6bae02744e0e11a5
Kanak-dev <kanak24.fv@gmail.com>
2024-03-24 23:41:16 +0545

    feat: infinite scroll

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const tagScrollerRight = document.querySelector('.tag-scroller-right');
 const navTagContainer = document.querySelector('.nav-tag-container');
 const tagsContainer = document.querySelector('.tags-container');
 const imgGallery = document.querySelector('.img-gallery');
-const imgGallerySubGrid1 = document.querySelector('.img-gallery-sub-grid-1');
-const imgGallerySubGrid2 = document.querySelector('.img-gallery-sub-grid-2');
-const imgGallerySubGrid3 = document.querySelector('.img-gallery-sub-grid-3');
+// const imgGallerySubGrid1 = document.querySelector('.img-gallery-sub-grid-1');
+// const imgGallerySubGrid2 = document.querySelector('.img-gallery-sub-grid-2');
+// const imgGallerySubGrid3 = document.querySelector('.img-gallery-sub-grid-3');
 
 const WIN_PADDING = 40;
 const SCROLL_UNIT = 200;
+let pageCount = 1;
 
 // hamburgerIcon.addEventListener('click', () => {
 //     hamburgerIcon.classList.toggle('ham-active');
@@ -55,20 +56,53 @@ tagScrollerLeft.addEventListener('click', () => {
 
 let images = [];
 const BASE_URL = 'https://api.unsplash.com/'
-const REQUEST_URL = `${BASE_URL}photos/?client_id=U-JKAdSdHZRA2-glU6Oe4WSzqHGP6GpKM8DZ8yUkelY`;
+const REQUEST_URL = `${BASE_URL}photos/?client_id=U-JKAdSdHZRA2-glU6Oe4WSzqHGP6GpKM8DZ8yUkelY&page=4`;
 
-const fetchData = async () => {
-    const response = await fetch(REQUEST_URL);
-    const data = await response.json();
-    return data;
+let target;
+
+const fetchData = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error("Error while fetching the data: ", err);
+        return;
+    }
 }
 
-const renderImages = async () => {
-    const images = await fetchData();
+const observer = new IntersectionObserver(entries => {
+    // if (entries[0].isIntersecting) {
+    //     pageCount ++;
+
+    // }
+    entries.forEach((entry, idx) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            renderImages(`${BASE_URL}photos/?client_id=U-JKAdSdHZRA2-glU6Oe4WSzqHGP6GpKM8DZ8yUkelY&page=${pageCount}`);
+
+            observer.unobserve(target);
+            pageCount ++;
+        }
+    })
+    console.log(entries);
+});
+
+const renderImages = async (url) => {
+    let newImages = [];
+    
+    if (images.length === 0) {
+        newImages = await fetchData(url);
+        images = [...newImages];
+    } else {
+        // observer.unobserve(target);
+        newImages = [...await fetchData(url)];
+        images = [...images, ...newImages];
+    }
     console.log(images);
 
     // filter out only the needed data...
-    const requiredImageData = images.map(item => ({
+    const requiredImageData = newImages.map(item => ({
         id: item.id,
         user: item.user,
         description: item.description,
@@ -113,19 +147,18 @@ const renderImages = async () => {
         imgContainer.appendChild(imgOverlay);
         imgContainer.appendChild(imgElement);
 
-        if (i < 4) {
-            imgGallerySubGrid1.appendChild(imgContainer);
-        } else if (i < 8) {
-            imgGallerySubGrid2.appendChild(imgContainer);
-        } else {
-            imgGallerySubGrid3.appendChild(imgContainer);
-        }
-        // imgGallery.appendChild(imgContainer);
+        imgGallery.appendChild(imgContainer);
     }
     
-    imgGallery.appendChild(imgGallerySubGrid1);
-    imgGallery.appendChild(imgGallerySubGrid2);
-    imgGallery.appendChild(imgGallerySubGrid3);
+    // imgGallery.appendChild(imgGallerySubGrid1);
+    // imgGallery.appendChild(imgGallerySubGrid2);
+    // imgGallery.appendChild(imgGallerySubGrid3);
+
+    // target = imgGallerySubGrid3.lastChild;
+    target = imgGallery.lastChild;
+    console.log(target);
+
+    observer.observe(target);
 }
 
-renderImages();
+// renderImages(REQUEST_URL);
